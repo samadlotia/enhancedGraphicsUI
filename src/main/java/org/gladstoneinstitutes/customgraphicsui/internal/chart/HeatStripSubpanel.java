efa8f46c3383b5fde6ed37afdc9606a10db2dae4
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2013-11-02 11:57:56 -0700

    Completed heat strip subpanel

--- src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/chart/HeatStripSubpanel.java
+++ src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/chart/HeatStripSubpanel.java
@@ -5,9 +5,11 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.Color;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.JSlider;
@@ -21,6 +23,9 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.view.model.CyNetworkView;
@@ -28,6 +33,7 @@ import org.cytoscape.view.model.View;
 
 import org.gladstoneinstitutes.customgraphicsui.internal.util.EasyGBC;
 import org.gladstoneinstitutes.customgraphicsui.internal.util.Strings;
+import org.gladstoneinstitutes.customgraphicsui.internal.util.ColorEditorPanel;
 
 class HeatStripSubpanel extends ChartSubpanel {
   final NumericAttributesTable attrsTable = new NumericAttributesTable();
@@ -66,17 +72,61 @@ class HeatStripSubpanel extends ChartSubpanel {
     separationPanel.add(new JLabel("Space between strips:"));
     separationPanel.add(separationSpinner);
 
-    final EasyGBC c = new EasyGBC();
-    final JPanel tricolorsPanel = new JPanel(new GridBagLayout());
 
+    final JRadioButton positiveButton = new JRadioButton("Positive");
+    final JRadioButton zeroButton = new JRadioButton("Zero");
+    final JRadioButton negativeButton = new JRadioButton("Negative");
+
+    final ColorEditorPanel colorEditorPanel = new ColorEditorPanel();
+    colorEditorPanel.addPropertyChangeListener(ColorEditorPanel.COLOR_CHANGED, new PropertyChangeListener() {
+      public void propertyChange(final PropertyChangeEvent e) {
+        final Color newColor = colorEditorPanel.getColor();
+        if (positiveButton.isSelected())
+          positiveColor = newColor;
+        else if (zeroButton.isSelected())
+          zeroColor = newColor;
+        else if (negativeButton.isSelected())
+          negativeColor = newColor;
+        HeatStripSubpanel.this.firePropertyChange(CG_CHANGED, null, null);
+      }
+    });
+
+    positiveButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        colorEditorPanel.setColor(positiveColor);
+      }
+    });
+    zeroButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        colorEditorPanel.setColor(zeroColor);
+      }
+    });
+    negativeButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        colorEditorPanel.setColor(negativeColor);
+      }
+    });
+
+    final ButtonGroup tricolorsGroup = new ButtonGroup();
+    tricolorsGroup.add(positiveButton);
+    tricolorsGroup.add(zeroButton);
+    tricolorsGroup.add(negativeButton);
+
+    final JPanel tricolorsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    tricolorsPanel.add(new JLabel("Colors: "));
+    tricolorsPanel.add(positiveButton);
+    tricolorsPanel.add(zeroButton);
+    tricolorsPanel.add(negativeButton);
 
     final JPanel optionsPanel = new JPanel(new GridLayout(0, 1, 0, 0));
     optionsPanel.add(labelPanel);
     optionsPanel.add(separationPanel);
     optionsPanel.add(tricolorsPanel);
 
+    final EasyGBC c = new EasyGBC();
     super.add(new JScrollPane(attrsTable), c.reset().expandHV());
     super.add(optionsPanel, c.anchor("nw").down().noExpand());
+    super.add(colorEditorPanel, c.down().expandHV());
   }
 
   public String getUserName() {
--- src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/gradient/GradientPanel.java
+++ src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/gradient/GradientPanel.java
@@ -47,27 +47,27 @@ public class GradientPanel extends JPanel {
   final CustomGraphicsFactoryManager manager;
   final GradientEditor editor;
   final GradientOrientationEditor gradientPositionEditor;
-  final ColorEditorPanel ColorEditorPanel;
+  final ColorEditorPanel colorEditorPanel;
   final AnchorPositionPanel anchorPositionPanel;
 
   public GradientPanel(final CustomGraphicsFactoryManager manager) {
     this.manager = manager;
     this.editor = new GradientEditor();
     this.gradientPositionEditor = new GradientOrientationEditor(editor, manager);
-    this.ColorEditorPanel = new ColorEditorPanel();
+    this.colorEditorPanel = new ColorEditorPanel();
     this.anchorPositionPanel = new AnchorPositionPanel();
 
     editor.addPropertyChangeListener(editor.SELECTED_STOP_CHANGED, new PropertyChangeListener() {
       public void propertyChange(final PropertyChangeEvent e) {
         final Gradient.Stop selectedStop = editor.getSelectedStop();
-        ColorEditorPanel.setColor(selectedStop == null ? null : selectedStop.getColor());
+        colorEditorPanel.setColor(selectedStop == null ? null : selectedStop.getColor());
         anchorPositionPanel.setPosition(selectedStop == null ? null : selectedStop.getPosition());
       }
     });
 
-    ColorEditorPanel.addPropertyChangeListener(ColorEditorPanel.COLOR_CHANGED, new PropertyChangeListener() {
+    colorEditorPanel.addPropertyChangeListener(ColorEditorPanel.COLOR_CHANGED, new PropertyChangeListener() {
       public void propertyChange(final PropertyChangeEvent e) {
-        final Color newColor = ColorEditorPanel.getColor();
+        final Color newColor = colorEditorPanel.getColor();
         final Gradient.Stop selectedStop = editor.getSelectedStop();
         if (selectedStop != null) {
           selectedStop.setColor(newColor);
@@ -99,7 +99,7 @@ public class GradientPanel extends JPanel {
 
     final JPanel anchorPanel = new JPanel(new GridBagLayout());
     final EasyGBC c = new EasyGBC();
-    anchorPanel.add(ColorEditorPanel, c);
+    anchorPanel.add(colorEditorPanel, c);
     anchorPanel.add(anchorPositionPanel, c.anchor("w").down().insets(10, 0, 0, 0));
 
     gradientPositionEditor.setBorder(BorderFactory.createLineBorder(new Color(0x858585), 1));
