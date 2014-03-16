8230376c737f90da9deca0ceed6131833a5be45e
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-03-16 16:44:10 -0700

    Added Scooter's arg map parsing code

--- src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/util/Strings.java
+++ src/main/java/org/gladstoneinstitutes/customgraphicsui/internal/util/Strings.java
@@ -2,6 +2,14 @@ package org.gladstoneinstitutes.customgraphicsui.internal.util;
 
 import java.awt.Color;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.StringReader;
+import java.io.StreamTokenizer;
+
 public class Strings {
 	public static String join(String[] pieces, String joiner) {
     final StringBuffer buffer = new StringBuffer();
@@ -25,5 +33,52 @@ public class Strings {
       c.getGreen(),
       c.getBlue(),
       c.getAlpha());
+  }
+
+  public static Map<String,String> toArgMap(String input) {
+    Map<String,String> settings = new HashMap<String,String>();
+
+    StringReader reader = new StringReader(input);
+    StreamTokenizer st = new StreamTokenizer(reader);
+
+    st.ordinaryChar('/');
+    st.ordinaryChar('_');
+    st.ordinaryChar('-');
+    st.ordinaryChar('.');
+    st.ordinaryChars('0', '9');
+
+    st.wordChars('/', '/');
+    st.wordChars('_', '_');
+    st.wordChars('-', '-');
+    st.wordChars('.', '.');
+    st.wordChars('0', '9');
+
+    List<String> tokenList = new ArrayList();
+    int tokenIndex = 0;
+    int i;
+    try {
+      while ((i = st.nextToken()) != StreamTokenizer.TT_EOF) {
+        switch(i) {
+          case '=':
+            i = st.nextToken();
+            if (i == StreamTokenizer.TT_WORD || i == '"') {
+              tokenIndex--;
+              String key = tokenList.get(tokenIndex);
+              settings.put(key, st.sval);
+              tokenList.remove(tokenIndex);
+            }
+            break;
+          case '"':
+          case StreamTokenizer.TT_WORD:
+            tokenList.add(st.sval);
+            tokenIndex++;
+            break;
+          default:
+            break;
+        }
+      }
+    } catch (Exception e) { return null; }
+
+    return settings;
   }
 }
